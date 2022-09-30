3b31e826d74f0bd18515820f6af3ff27ee45dee7
Akhil Bisht <akhilbisht798@gmail.com>
2022-09-30 17:36:48 +0530

    Sidebar UI

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -20,7 +20,7 @@ export default class UI {
         if (project.projectName() != "Inbox" && project.projectName() != "Today" && project.projectName() != "This Week") {
             div2.classList.add("temp_Projects"); // Can be deleted.
             div2.innerHTML = ` <div class="pname">${project.projectName()}</div>
-                                <button class="deletebtn" onclick="UI.DeleteProjectUI(this)" >Delete</button>`
+                                <button class="deletebtn" >Delete</button>`
         }
         else {
             div2.innerHTML = project.projectName();
@@ -39,6 +39,7 @@ export default class UI {
             SideBar.appendChild(this.MakeProjectItems(project));
         })
 
+        UI.DeleteProjectUI();
     }
 
     static CreatePopupBtn() {
@@ -58,9 +59,11 @@ export default class UI {
 
         let addbtn = document.getElementById("project-btn-div");
         let div = document.createElement('div');
+        let AddButton = document.getElementById("project-add-btn");
         div.id = "INput-Project-Pop";
         div.innerHTML = `<input type="text" name="input_Project_Name" id="input-Project-Name">`;
         const delBtn = document.createElement('button');
+        delBtn.addEventListener('click', UI.PopUpCancelBtn)
         delBtn.innerHTML = "Cancel";
 
         const addBtn = document.createElement('button')
@@ -69,23 +72,39 @@ export default class UI {
 
         div.appendChild(addBtn);
         div.appendChild(delBtn);
-
+        AddButton.disabled = true;
         addbtn.appendChild(div);
     }
 
     static createProjectObj() {
 
         let text = document.getElementById('input-Project-Name').value;
-        console.log(text);
+        if (text.length === 0) {
+            return;
+        }
         Storage.saveNewProject(text);
         UI.UpdateProjectList();
     }
 
-    static DeleteProjectUI(elem) {
+    static DeleteProjectUI() {
 
-        let text = elem.target.parentNode.firstChild.innerHTML;
-        Storage.StoredeleteProject(text);
-        UI.UpdateProjectList();
+        let deletebtn = document.querySelectorAll('.deletebtn');
+        deletebtn.forEach((btn) => {
+            btn.addEventListener('click', (e) => {
+                let parent = e.target.parentNode.children;
+                let text = parent[0].innerHTML;
+                Storage.StoredeleteProject(text);
+                UI.UpdateProjectList();
+            })
+        })
     }
 
+    static PopUpCancelBtn() {
+        let AddButton = document.getElementById("project-add-btn");
+        let div = document.getElementById("INput-Project-Pop");
+        div.remove();
+        AddButton.disabled = false;
+    }
+
+    //Last Thing in Sidebar dont allow more popup for input.
 }
