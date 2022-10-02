b02c2b9ba52662df8320f5831afbbc0b05d48d20
Akhil Bisht <akhilbisht798@gmail.com>
2022-10-02 11:34:00 +0530

    delete task working

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import task from './modules/task'
 import Todolist from './modules/Todolist'
 import Storage from './modules/Storage'
 import UI from './modules/UI'
+import './style/sidebar.css';
 
+Storage.addNewTask("This Week", "Gra");
+Storage.addNewTask("Inbox", "THe");
+Storage.addNewTask("Inbox", "THee");
+Storage.addNewTask("Inbox", "T");
+Storage.addNewTask("Inbox", "H");
 UI.loadProjects();
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -4,6 +4,8 @@ import Todolist from './Todolist';
 
 export default class UI {
 
+    static curr = "Inbox";
+
     static loadProjects() {
         let container = document.querySelector(".container");
 
@@ -12,6 +14,7 @@ export default class UI {
         container.appendChild(div);
         this.UpdateProjectList();
 
+        UI.loadTodos();
     }
 
     static MakeProjectItems(project) {
@@ -20,11 +23,11 @@ export default class UI {
         if (project.projectName() != "Inbox" && project.projectName() != "Today" && project.projectName() != "This Week") {
             div2.classList.add("temp_Projects"); // Can be deleted.
             div2.innerHTML = ` <div class="pname">${project.projectName()}</div>
-                                <button class="deletebtn" >Delete</button>`
+                                <button class="deletebtn" > x </button>`
         }
         else {
             div2.innerHTML = project.projectName();
-            div2.classList.add('projects');
+            div2.classList.add('pname');
         }
         return div2;
     }
@@ -40,6 +43,7 @@ export default class UI {
         })
 
         UI.DeleteProjectUI();
+        UI.ClickProject();
     }
 
     static CreatePopupBtn() {
@@ -68,7 +72,7 @@ export default class UI {
 
         const addBtn = document.createElement('button')
         addBtn.addEventListener('click', UI.createProjectObj);
-        addBtn.innerHTML = "ADD";
+        addBtn.innerHTML = "Add";
 
         div.appendChild(addBtn);
         div.appendChild(delBtn);
@@ -106,5 +110,82 @@ export default class UI {
         AddButton.disabled = false;
     }
 
-    //Last Thing in Sidebar dont allow more popup for input.
+    static loadTodos() {
+        let div = document.createElement('div');
+        div.id = "Todos";
+        let container = document.querySelector(".container");
+        container.appendChild(div);
+    }
+
+    static ClickProject() {
+        let pname = document.querySelectorAll('.pname');
+        pname.forEach((p) => {
+            p.addEventListener('click', (e) => {
+                let text = e.target.innerHTML;
+                UI.curr = text;
+                UI.GetTodos(text);
+            })
+        })
+    }
+
+    static GetTodos(text) {
+        let Todos = document.getElementById('Todos');
+        Todos.innerHTML = "";
+        const Todo = Storage.getTodoList();
+        const p = Todo.findProject(text);
+        p.task.forEach(t => {
+            UI.CreateTodo(t);
+        });
+        UI.deleteTodo();
+    }
+
+    static CreateTodo(todo) {
+        let Todos = document.getElementById('Todos');
+        let div = document.createElement('div');
+        div.classList.add("Indiviual_todo")
+        div.innerHTML = `<div class="Todo-name">${todo.taskName()}</div>
+        <input type="checkbox" class="Is-Complete">`
+        Todos.appendChild(div);
+    }
+
+    static deleteTodo() {
+        let Is_complete = document.querySelectorAll('.Is-Complete')
+        Is_complete.forEach((c) => {
+            c.addEventListener('click', (e) => {
+                let parent = e.target.parentNode.children;
+                let text = parent[0].innerHTML;
+                Storage.deleteTask(UI.curr, text);
+                UI.GetTodos(UI.curr);
+            })
+        })
+    }
+
+    // Create a Add Todo btn.
+    // figure out time and date.
+    //Make popup for input task.
+    //Make popup for onclicking tasks to get imformation.
+    static AddTaskBtn() {
+        let Todos = document.getElementById('Todos');
+        let btn = document.createElement('button');
+        btn.innerHTML = "New Task";
+        btn.addEventListener('click', UI.NewTaskPopUPOpen);
+        Todos.appendChild(btn);
+    }
+
+    static POPUPNewTask() {
+        let div = document.createElement('div');
+        div.classList.add("modal-container");
+        div.innerHTML = `
+        <div class="modal">
+        <h1>thIS IS Awesome 😎</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque dolore fuga totam, sunt vitae inventore optio
+            saepe suscipit expedita omnis?</p>
+        </div>`;
+        return div;
+    }
+
+    static NewTaskPopUPOpen() {
+        let Todos = document.getElementById('Todos');
+        Todos.appendChild(UI.POPUPNewTask())
+    }
 }
